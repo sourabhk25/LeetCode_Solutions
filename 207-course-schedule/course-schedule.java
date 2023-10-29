103ceb2ddd90ff6cfe4e85f442a580492508bb98
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2023-10-29 15:10:13 -0700

    Time: 5 ms (66.21%) | Memory: 43.8 MB (67.89%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,37 +1,38 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new ArrayList<>();
-        int[] indegree = new int[numCourses];
+        //Topological sort Kahn's algorithm
+        List<List<Integer>> adj = new ArrayList<>();    //adjacency list
+        int[] indegree = new int[numCourses];   //indegree of all nodes
 
-        for(int i=0; i<numCourses; i++) {
+        for(int i=0; i<numCourses; i++) {   //creating adjacency list
             adj.add(new ArrayList<>());
         }
 
-        for(int[] prereq: prerequisites) {
+        for(int[] prereq: prerequisites) {  //loop through prerequisites and add edges
             adj.get(prereq[1]).add(prereq[0]);
-            indegree[prereq[0]]++;
+            indegree[prereq[0]]++;  //update indegrees
         }
 
-        Queue<Integer> queue = new LinkedList<>();
-        for(int i=0; i<numCourses; i++) {
+        Queue<Integer> queue = new LinkedList<>();  //queue for topo sort
+        for(int i=0; i<numCourses; i++) {   //add nodes with 0 indegree in queue to start BFS
             if(indegree[i] == 0) {
                 queue.add(i);
             }
         }
 
-        int visitedCount = 0;
-        while(!queue.isEmpty()) {
-            int node = queue.poll();
-            visitedCount++;
+        int visitedCount = 0;   //count for visited nodes
+        while(!queue.isEmpty()) {   //loop till queue is not empty
+            int node = queue.poll();    //remove front node
+            visitedCount++; //increase visited count
 
-            for(int neighbor: adj.get(node)) {
-                indegree[neighbor]--;
-                if(indegree[neighbor] == 0) {
-                    queue.offer(neighbor);
+            for(int neighbor: adj.get(node)) {  //loop in adjacency list of node
+                indegree[neighbor]--;   //decrease indegree of neighbor
+                if(indegree[neighbor] == 0) {   //if indegree becomes 0 add to queue
+                    queue.add(neighbor);
                 }
             }
         }
 
-        return visitedCount == numCourses;
+        return visitedCount == numCourses;  //return if visitedCount is same as numCourses
     }
 }
