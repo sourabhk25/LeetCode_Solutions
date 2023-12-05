0f84152ff452f1234867d44a908d3c78b84ef0a6
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2023-12-04 19:35:00 -0800

    Time: 1 ms (94.28%) | Memory: 44.1 MB (16.95%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,26 +1,26 @@
 class Solution {
-    private void __permute(int[] nums, List<List<Integer>> ans, List<Integer> ds, boolean[] freq) {
-        if(ds.size() == nums.length) {
-            ans.add(new ArrayList<>(ds));
+    private void __permute(int[] nums, List<List<Integer>> ans, List<Integer> ds, boolean[] visited) {
+        if(ds.size() == nums.length) {  //check if ds has same size as array
+            ans.add(new ArrayList<>(ds));   //add ds contents i.e. permutation to ans
             return;
         }
         
-        for(int i=0; i<nums.length; i++) {
-            if(!freq[i]) {
-                freq[i] = true;
-                ds.add(nums[i]);
-                __permute(nums, ans, ds, freq); //recursive call
+        for(int i=0; i<nums.length; i++) {  //run for all elements in array
+            if(!visited[i]) {  //check if ith element visited or not
+                visited[i] = true;  //mark visited
+                ds.add(nums[i]);    //add num to ds
+                __permute(nums, ans, ds, visited); //recursive call
                 ds.remove(ds.size() - 1);   //remove last element after finishing call
-                freq[i] = false;
+                visited[i] = false; //mark visited as false after call
             }
         }
     }
     
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        boolean[] freq = new boolean[nums.length];
+        boolean[] visited = new boolean[nums.length];
         List<Integer> ds = new ArrayList<>();
-        __permute(nums, ans, ds, freq);
+        __permute(nums, ans, ds, visited);
         return ans;
     }
 }
