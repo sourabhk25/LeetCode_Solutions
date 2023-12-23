f09aadf53885b818dbb2e8b70dfbbcc7bfd92540
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2023-12-23 01:23:18 -0800

    Time: 0 ms (100.00%) | Memory: 46.7 MB (5.26%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -2,31 +2,31 @@ class Solution {
     public int trap(int[] height) {
         //2 pointer approach
         int n = height.length;
-        if(n == 0)  return 0;
-        int left = 0, right = n - 1;
-        int leftmax = height[0];
-        int rightmax = height[n - 1];
-        int ans = 0;
+        if(n == 0)  return 0;   //return 0 if empty input array
+        int left = 0, right = n - 1;    //setting 2 pointers
+        int leftmax = height[0];    //max height from left
+        int rightmax = height[n - 1];   //max height from right
+        int ans = 0;    //ans variable
         
-        while(left <= right) {
+        while(left <= right) {  //loop till left and right don't cross each other
             //check which side has minimum height
-            if(leftmax < rightmax) {
-                if(height[left] > leftmax) {
-                    leftmax = height[left];
+            if(leftmax < rightmax) {    //if left side has smaller 
+                if(height[left] > leftmax) {    //check left bar height is greater than current leftmax
+                    leftmax = height[left]; //change if yes
                 } else {
-                    ans += leftmax - height[left];
-                    left++;
+                    ans += leftmax - height[left];  //else add difference to ans
+                    left++; //increment left ptr
                 }
-            } else {
-                if(height[right] > rightmax) {
-                    rightmax = height[right];
+            } else {    //right side has smaller 
+                if(height[right] > rightmax) {  //check right bar height is greater than current rightmax
+                    rightmax = height[right];   //change if yes
                 } else {
-                    ans += rightmax - height[right];
-                    right--;
+                    ans += rightmax - height[right];    //else add difference to ans
+                    right--;    //increment right ptr
                 }
             }
         }
 
-        return ans;
+        return ans; //return ans
     }
 }
