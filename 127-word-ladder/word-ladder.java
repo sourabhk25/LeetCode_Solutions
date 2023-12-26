0b784608df1e252a2af5c3ed1dc6244a7011aae2
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2023-12-25 18:55:57 -0800

    Time: 97 ms (40.19%) | Memory: 46.7 MB (38.19%) - LeetSync

--- 127-word-ladder/word-ladder.java
+++ 127-word-ladder/word-ladder.java
@@ -1,7 +1,7 @@
 class Pair {
-    String first;
-    int second;
-    Pair(String s, int i) {
+    String first;   //store word
+    int second;     //store step no
+    Pair(String s, int i) {     //constructor
         first = s;
         second = i;
     }
@@ -9,33 +9,34 @@ class Pair {
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
         Queue<Pair> q = new LinkedList<>(); //Queue to store word transformation and steps
-        q.offer(new Pair(beginWord, 1));
+        q.offer(new Pair(beginWord, 1));    //add beginWord to queue to start BFS
         Set<String> st = new HashSet<>();   //to unmark visited words from wordList
-        for(String w: wordList) {
+        for(String w: wordList) {   //add words to set for better lookup
             st.add(w);
         }
         st.remove(beginWord);   //remove start word since visited
 
-        while(!q.isEmpty()) {
-            Pair p = q.poll();
+        while(!q.isEmpty()) {   //loop till q is empty
+            Pair p = q.poll();  //get front element and fetch data members
             String word = p.first;
             int steps = p.second;
-            if(word.equals(endWord)) {
+            if(word.equals(endWord)) {  //if word is same as endWord return steps
                 return steps;
             }
-            for(int i = 0; i < word.length(); i++) {
-                for(char ch = 'a'; ch <= 'z'; ch++) {
-                    char[] wordArray = word.toCharArray();
+            //transformation steps
+            for(int i = 0; i < word.length(); i++) {    //loop through all char of word
+                for(char ch = 'a'; ch <= 'z'; ch++) {   //loop change each char from a to z
+                    char[] wordArray = word.toCharArray();  
                     wordArray[i] = ch;
                     String tempWord = new String(wordArray);
-                    if(st.contains(tempWord)) {
-                        st.remove(tempWord);
-                        q.offer(new Pair(tempWord, steps + 1));
+                    if(st.contains(tempWord)) {     //if set contains tempWord
+                        st.remove(tempWord);    //remove it
+                        q.offer(new Pair(tempWord, steps + 1)); //add to queue with steps+1
                     }
                 }
             }
         }
 
-        return 0;        
+        return 0;   //return 0 if could not be converted
     }
 }
