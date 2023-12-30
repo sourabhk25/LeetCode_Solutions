64ad97472d3006bb10f2368883de839605b6c935
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2023-12-29 20:56:22 -0800

    Time: 2 ms (59.50%) | Memory: 45 MB (6.54%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,16 +1,16 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int n = nums.length;
-        int prefix = 1, suffix = 1;
-        int ans = Integer.MIN_VALUE;
-        for(int i = 0; i < n; i++) {
-            if(prefix == 0)     prefix = 1;
-            if(suffix == 0)     suffix = 1;
-            prefix *= nums[i];
-            suffix *= nums[n - i - 1];
-            ans = Math.max(ans, Math.max(prefix, suffix));
+        int n = nums.length;    //get no of elements
+        int prefix = 1, suffix = 1; //initialize prefix and suffix multiplication values
+        int ans = Integer.MIN_VALUE;    //initialize ans 
+        for(int i = 0; i < n; i++) {    //loop through nums array
+            if(prefix == 0)     prefix = 1; //if prefix becomes 0 then reset it to 1
+            if(suffix == 0)     suffix = 1; //if suffix becomes 0 then reset it to 1
+            prefix *= nums[i];  //calculate prefix multiplication from left direction
+            suffix *= nums[n - i - 1];  //calculate suffix multiplication from right direction
+            ans = Math.max(ans, Math.max(prefix, suffix));  //store max ans
         }
 
-        return ans;
+        return ans; //return ans
     }
 }
