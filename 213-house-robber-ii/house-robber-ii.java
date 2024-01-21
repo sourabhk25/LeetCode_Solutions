638ca66bcc7faa19aa32290a35a6a654e4298f55
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2024-01-20 20:18:47 -0800

    Time: 0 ms (100.00%) | Memory: 41.1 MB (24.93%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,31 @@
+class Solution {
+    
+    private int houseRobber_I(int[] nums) {
+        int n = nums.length;    //getting size of input array
+        if(n == 0)    return 0; //return 0 if empty input array
+        
+        int[] dp = new int[n + 1];  //creating array to store max money robbed so far
+        dp[0] = 0;  //initialize to 0 since no houses to rob so far
+        dp[1] = nums[0];    //initialize to nums[0] since 1st house is robbed
+        
+        for(int i = 1; i < n; i++) {    //loop through all remaining houses
+            dp[i + 1] = Math.max(dp[i], dp[i - 1] + nums[i]);   //calculate max money so far as maximum of current maxValueRobbed from houses and sum of alternate house and current house money
+        }
+        
+        return dp[n];   //return max money robbed
+    }
+    
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n == 0)  return 0; //return 0 if empty input array
+        if(n == 1)  return nums[0];
+        int[] first = new int[n - 1];
+        int[] second = new int[n - 1];
+        for(int i = 0; i < n; i++) {
+            if(i != n - 1)  first[i] = nums[i];
+            if(i != 0)  second[i - 1] = nums[i];
+        }
+
+        return Math.max(houseRobber_I(first), houseRobber_I(second));
+    }
+}
