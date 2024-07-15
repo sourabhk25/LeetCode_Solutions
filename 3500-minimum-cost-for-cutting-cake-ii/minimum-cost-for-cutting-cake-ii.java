69f168e0ee9e9a715e0e0fc2bae3206647d17cb4
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2024-07-15 12:09:12 -0700

    Time: 344 ms (33.33%) | Memory: 60.3 MB (66.67%) - LeetSync

--- 3500-minimum-cost-for-cutting-cake-ii/minimum-cost-for-cutting-cake-ii.java
+++ 3500-minimum-cost-for-cutting-cake-ii/minimum-cost-for-cutting-cake-ii.java
@@ -1,45 +1,42 @@
 class Solution {
     public long minimumCost(int m, int n, int[] horizontalCut, int[] verticalCut) {
-        if(m == 0 && n == 0) {
+        if (m == 0 && n == 0) {
             return 0;
         }
-        // Convert int[] to Integer[] to use Collections.reverseOrder()
+        
+        // Convert primitive int arrays to Integer arrays for sorting with Collections
         Integer[] hCuts = Arrays.stream(horizontalCut).boxed().toArray(Integer[]::new);
         Integer[] vCuts = Arrays.stream(verticalCut).boxed().toArray(Integer[]::new);
         
-        // Sort the cuts in descending order of cost
+        // Sort cuts in descending order of cost
         Arrays.sort(hCuts, Collections.reverseOrder());
         Arrays.sort(vCuts, Collections.reverseOrder());
         
-        // Priority queues to hold the cuts, initially sorted by cost
-        PriorityQueue<Integer> hQueue = new PriorityQueue<>(Arrays.asList(hCuts));
-        PriorityQueue<Integer> vQueue = new PriorityQueue<>(Arrays.asList(vCuts));
-        
         int hIndex = 0, vIndex = 0;
-        int hCount = 1, vCount = 1;
+        int hCount = 1, vCount = 1;  // Start with 1 segment
         long totalCost = 0;
         
+        // Process both lists of cuts
         while (hIndex < hCuts.length && vIndex < vCuts.length) {
+            // Choose the more expensive cut to minimize total cost
             if (hCuts[hIndex] >= vCuts[vIndex]) {
-                totalCost += hCuts[hIndex] * vCount;
+                totalCost += hCuts[hIndex] * vCount;  // Multiply by the number of vertical segments
                 hIndex++;
                 hCount++;
             } else {
-                totalCost += vCuts[vIndex] * hCount;
+                totalCost += vCuts[vIndex] * hCount;  // Multiply by the number of horizontal segments
                 vIndex++;
                 vCount++;
             }
         }
         
-        // Process remaining horizontal cuts
         while (hIndex < hCuts.length) {
-            totalCost += hCuts[hIndex] * vCount;
+            totalCost += hCuts[hIndex] * vCount;  // Multiply by the number of vertical segments
             hIndex++;
         }
         
-        // Process remaining vertical cuts
         while (vIndex < vCuts.length) {
-            totalCost += vCuts[vIndex] * hCount;
+            totalCost += vCuts[vIndex] * hCount;  // Multiply by the number of horizontal segments
             vIndex++;
         }
         
