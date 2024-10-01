c74a049e39187f6ba6fc5b425fd7ebd3c9eedf5e
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2024-10-01 16:01:00 -0700

    Time: 45 ms (15.80%) | Memory: 58 MB (18.50%) - LeetSync

---
+++ 1620-check-if-array-pairs-are-divisible-by-k/check-if-array-pairs-are-divisible-by-k.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canArrange(int[] arr, int k) {
+        //VIMP - for negative and positive nos both module you need to use (x % k + k) % k
+        Map<Integer, Integer> remainderCount = new HashMap<>(); //store remainder and count of that remainder in array
+        for(int x: arr) {
+            int rem = ((x % k) + k) % k;
+            remainderCount.put(rem, remainderCount.getOrDefault(rem, 0) + 1);
+        }
+        System.out.println(arr.length);
+        //now go through array again, find remainder again
+        //if remainder is 0 then check if count of these nos is even if not return false
+        //else if count of rem and count of k - rem is not same then also return false because we cannot make all pairs
+        //use Objects.equal method for checking it correctly
+        //at the end return true
+        for(int x: arr) {
+            int rem = ((x % k) + k) % k;
+            if(rem == 0) {
+                if(remainderCount.get(rem) % 2 == 1) {
+                    return false;
+                }
+            } else if(!Objects.equals(remainderCount.get(rem), remainderCount.get(k - rem))) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
