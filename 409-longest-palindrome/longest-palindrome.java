f8e3be60ace6aef055ce26a79d0aa26b8eae84f4
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-03-18 22:43:29 -0500

    Time: 5 ms (73.44%) | Memory: 42.3 MB (18.02%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.java
+++ 409-longest-palindrome/longest-palindrome.java
@@ -1,18 +1,36 @@
 class Solution {
-    public int longestPalindrome(String s) {
-        int[] frequency = new int[128]; //128 due to ascii 
-        for(char c: s.toCharArray()) {
-            frequency[c]++; //increase frequency
-        }
+    //2 pass - TC = O(2n), SC = O(128)
+    // public int longestPalindrome(String s) {
+    //     int[] frequency = new int[128]; //128 due to ascii 
+    //     for(char c: s.toCharArray()) {
+    //         frequency[c]++; //increase frequency
+    //     }
 
-        int result = 0;
-        for(int f:frequency) {
-            result += f / 2 * 2;    //increase result/max length of palindrome
-            if(result % 2 == 0 && f % 2 == 1) { //check if centre char is added to palindrom or not
-                result++;
+    //     int result = 0;
+    //     for(int f:frequency) {
+    //         result += f / 2 * 2;    //increase result/max length of palindrome
+    //         if(result % 2 == 0 && f % 2 == 1) { //check if centre char is added to palindrom or not
+    //             result++;
+    //         }
+    //     }
+
+    //     return result;
+    // }
+
+    public int longestPalindrome(String s) {
+        HashSet<Character> hashSet = new HashSet<>();
+        int maxLength = 0;
+        for(char c: s.toCharArray()) {
+            if(hashSet.contains(c)) {
+                maxLength += 2;
+                hashSet.remove(c);
+            } else {
+                hashSet.add(c);
             }
         }
-
-        return result;
+        if(!hashSet.isEmpty()) {
+            maxLength++;
+        }
+        return maxLength;
     }
 }
