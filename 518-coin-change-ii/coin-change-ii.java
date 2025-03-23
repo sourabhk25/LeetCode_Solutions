9ad02e55e9ad4bc9021b884a0680e45288ca94b6
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-03-22 23:46:26 -0500

    Time: 8 ms (69.87%) | Memory: 48.9 MB (56.66%) - LeetSync

---
+++ 518-coin-change-ii/coin-change-ii.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int change(int amount, int[] coins) {
+        int m = coins.length;
+        int n = amount;
+        int[][] dp = new int[m+1][n+1]; //to store cnt of ways of making coin change
+
+        //initialize first 0 like with 0 coins and 0 amt, 1 ways possible, like 0 amt is possible with any no of coins so cnt is 1
+        dp[0][0] = 1;   //in recursssion, when amt reaches 0 then we incremented count
+
+        //loop through values of m and n in matrix
+        for(int i = 1; i <=m; i++) {
+            for(int j = 0; j <= n; j++) {
+                if(j < coins[i-1]) {    //amt less than coin denomination so no choose case
+                    dp[i][j] = dp[i-1][j];
+                } else {
+                    dp[i][j] = dp[i-1][j] + dp[i][j - coins[i - 1]];
+                }
+            }
+        }
+        return dp[m][n];
+    }
+}
