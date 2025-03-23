fbf58fef61e76021dd8c078573e3646ab849f630
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-03-23 00:56:37 -0500

    Time: 1 ms (89.11%) | Memory: 42.9 MB (72.40%) - LeetSync

--- 256-paint-house/paint-house.java
+++ 256-paint-house/paint-house.java
@@ -12,11 +12,13 @@ class Solution {
 
         //traverse costs and dp array from bottom to up to get the answers
         for(int i = m - 2; i >= 0; i--) {
+            //cost is calculated as cost at that place + min of cost of subtree of 2 other colors
             dp[i][0] = costs[i][0] + Math.min(dp[i + 1][1], dp[i + 1][2]);
             dp[i][1] = costs[i][1] + Math.min(dp[i + 1][0], dp[i + 1][2]);
             dp[i][2] = costs[i][2] + Math.min(dp[i + 1][0], dp[i + 1][1]);
         }
 
+        //return min from first row which contains total costs for each subtree for each color choice
         return Math.min(dp[0][0], Math.min(dp[0][1], dp[0][2]));
     }
 }
