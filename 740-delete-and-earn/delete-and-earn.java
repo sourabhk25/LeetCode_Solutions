bfb5412e8c6ea341360c8891c8ce715ff5aba632
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-03-29 17:05:50 -0500

    Time: 12 ms (35.37%) | Memory: 44.3 MB (87.79%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,23 +1,58 @@
 class Solution {
+    // public int deleteAndEarn(int[] nums) {
+    //     int max = 0;
+    //     for(int num: nums) {
+    //         max = Math.max(max, num);
+    //     }
+
+    //     //create array for points sum 
+    //     int[] pointsArr = new int[max + 1];
+    //     for(int num: nums) {
+    //         pointsArr[num] += num;
+    //     }
+
+    //     //now problem is like house robber problem
+    //     int prev = pointsArr[0]; //stores no choose values from previous 
+    //     int curr = Math.max(pointsArr[0], pointsArr[1]); //option of choosing current or prev 
+
+    //     for(int i = 2; i <= max; i++) {
+    //         int temp = curr;
+    //         curr = Math.max(curr, pointsArr[i] + prev);
+    //         prev = temp;
+    //     }
+
+    //     return curr;
+    // }
+
     public int deleteAndEarn(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int min = Integer.MAX_VALUE;
         int max = 0;
+        
         for(int num: nums) {
+            map.put(num, map.getOrDefault(num, 0) + num);
             max = Math.max(max, num);
+            min = Math.min(min, num);
         }
 
-        //create array for points sum 
-        int[] pointsArr = new int[max + 1];
-        for(int num: nums) {
-            pointsArr[num] += num;
+        //no need of input array anymore
+        //iterate through map now
+        //find prev and curr values as - 
+        int prev = map.get(min);
+        int curr = prev;
+        if(map.containsKey(min + 1)) {
+            curr = Math.max(prev, map.get(min + 1));
         }
-
-        //now problem is like house robber problem
-        int prev = pointsArr[0];
-        int curr = Math.max(pointsArr[0], pointsArr[1]);
-
-        for(int i = 2; i <= max; i++) {
+        
+        //iterate through range of min + 2  and max
+        //since we already created prev and curr earlier
+        for(int i = min + 2; i <= max; i++) {
             int temp = curr;
-            curr = Math.max(curr, pointsArr[i] + prev);
+            if(map.containsKey(i)) {
+                curr = Math.max(curr, map.get(i) + prev);
+            } else {
+                curr = Math.max(curr, 0 + prev);
+            }
             prev = temp;
         }
 
