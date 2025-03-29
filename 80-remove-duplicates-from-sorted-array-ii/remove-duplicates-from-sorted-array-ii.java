8e829c9581c240009885b2d6bf790268d2b9a36a
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-03-28 23:06:59 -0500

    Time: 0 ms (100.00%) | Memory: 46.8 MB (40.34%) - LeetSync

--- 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.java
@@ -1,16 +1,43 @@
 class Solution {
+    // public int removeDuplicates(int[] nums) {
+    //     if(nums.length <= 2) {
+    //         return nums.length;
+    //     }
+
+    //     int j = 2;
+    //     for(int i = 2; i < nums.length; i++) {
+    //         nums[j] = nums[i];
+    //         if(nums[j] != nums[j - 2]) {
+    //             j++;
+    //         }
+    //     }
+    //     return j;
+    // }
+
+//scalable solution
     public int removeDuplicates(int[] nums) {
-        if(nums.length <= 2) {
-            return nums.length;
+        int n = nums.length;
+        if(n <= 2) {
+            return n;
         }
 
-        int j = 2;
-        for(int i = 2; i < nums.length; i++) {
-            nums[j] = nums[i];
-            if(nums[j] != nums[j - 2]) {
-                j++;
+        int k = 2;  //no of duplicates allowed of a occurance
+        int slow = 1, fast = 1;
+        int count = 1;
+        
+        while(fast < n) {
+            if(nums[fast] == nums[fast - 1]) {
+                count++;
+            } else {
+                count = 1;
             }
+            if(count <= k) {
+                nums[slow] = nums[fast];
+                slow++;
+            }
+            fast++;
         }
-        return j;
+
+        return slow;
     }
 }
