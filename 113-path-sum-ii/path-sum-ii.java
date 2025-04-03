be2e4aa0328361f0e98f43885702ced971c4d802
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-04-02 22:55:02 -0500

    Time: 2 ms (16.91%) | Memory: 47.6 MB (5.55%) - LeetSync

---
+++ 113-path-sum-ii/path-sum-ii.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<List<Integer>> result;
+    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+        this.result = new ArrayList<>();
+        helper(root, targetSum, 0, new ArrayList<>());
+        return result;
+    }
+
+    private void helper(TreeNode root, int targetSum, int currSum, List<Integer> path) {
+        if(root == null) {
+            return;
+        }
+        currSum += root.val;
+        List<Integer> temp = new ArrayList<>(path); //create deep copy of previous path and don't touch parent path list
+        temp.add(root.val);
+
+        if(root.left == null && root.right == null) {
+            //leaf node so check if targetSum == currSum
+            if(currSum == targetSum) {
+                result.add(temp);
+            }
+        }
+
+        //traverse through left and right subtree but pass temp as a new parent path
+        helper(root.left, targetSum, currSum, temp);
+
+        helper(root.right, targetSum, currSum, temp);
+    }
+}
