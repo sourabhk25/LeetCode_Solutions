280b500a1fb37ee45f2a73e55901db6ad5b0b214
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-04-09 22:39:25 -0500

    Time: 0 ms (100.00%) | Memory: 41.4 MB (74.49%) - LeetSync

--- 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
+++ 1035-cousins-in-binary-tree/cousins-in-binary-tree.java
@@ -66,50 +66,78 @@ class Solution {
     // }
 
     //without parent queue, before processing children check if root has both children and if they are x or y or not, if yes then return false, else add them in queue
-    public boolean isCousins(TreeNode root, int x, int y) {
-        if(root == null){
-            return true;
-        }
-        Queue<TreeNode> q = new LinkedList<>();
+    // public boolean isCousins(TreeNode root, int x, int y) {
+    //     if(root == null){
+    //         return true;
+    //     }
+    //     Queue<TreeNode> q = new LinkedList<>();
         
-        q.add(root);
+    //     q.add(root);
         
-        boolean x_found = false, y_found = false;
+    //     boolean x_found = false, y_found = false;
         
-        while(!q.isEmpty()) {
-            int size = q.size();
-            for(int i = 0; i< size; i++) {
-                TreeNode curr = q.poll();
+    //     while(!q.isEmpty()) {
+    //         int size = q.size();
+    //         for(int i = 0; i< size; i++) {
+    //             TreeNode curr = q.poll();
                 
-                if(curr.left != null && curr.right != null) {   //has both children
-                    if((curr.left.val == x && curr.right.val == y) || (curr.left.val == y && curr.right.val == x)) {
-                        return false;
-                    }
-                }
-                if(curr.val == x) {
-                    x_found = true;
-                }
-                if(curr.val == y) {
-                    y_found = true;
-                }
+    //             if(curr.left != null && curr.right != null) {   //has both children
+    //                 if((curr.left.val == x && curr.right.val == y) || (curr.left.val == y && curr.right.val == x)) {
+    //                     return false;
+    //                 }
+    //             }
+    //             if(curr.val == x) {
+    //                 x_found = true;
+    //             }
+    //             if(curr.val == y) {
+    //                 y_found = true;
+    //             }
 
-                if(curr.left != null) {
-                    q.add(curr.left);
-                }
+    //             if(curr.left != null) {
+    //                 q.add(curr.left);
+    //             }
 
-                if(curr.right != null) {
-                    q.add(curr.right);
-                }
-            }
+    //             if(curr.right != null) {
+    //                 q.add(curr.right);
+    //             }
+    //         }
 
-            if(x_found && y_found) {
-                return true;
-            }
-            if(x_found || y_found) {
-                return false;
-            }
+    //         if(x_found && y_found) {
+    //             return true;
+    //         }
+    //         if(x_found || y_found) {
+    //             return false;
+    //         }
+    //     }
+
+    //     return true;
+    // }
+
+    //DFS - space O(h) time O(n)
+    int x_level, y_level;
+    TreeNode x_parent, y_parent;
+    public boolean isCousins(TreeNode root, int x, int y) {
+        helper(root, x, y, 0, null);
+        return x_level == y_level && x_parent != y_parent;
+    }
+
+    private void helper(TreeNode root, int x, int y, int level, TreeNode parent) {
+        if(root == null || (x_parent != null && y_parent != null)) {
+            return; //if root is null or both x and y's parents found then return
         }
 
-        return true;
+        if(root.val == x) {
+            x_level = level;
+            x_parent = parent;
+        }
+
+        if(root.val == y) {
+            y_level = level;
+            y_parent = parent;
+        }
+
+        helper(root.left, x, y, level + 1, root);
+
+        helper(root.right, x, y, level + 1, root);
     }
 }
