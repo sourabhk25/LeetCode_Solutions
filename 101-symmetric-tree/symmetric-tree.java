cc13e1ea5bc19be35294bc1d678591b2d83d43d6
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-04-09 23:08:28 -0500

    Time: 1 ms (13.65%) | Memory: 42.1 MB (30.10%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -14,22 +14,54 @@
  * }
  */
 class Solution {
+    //DFS
+    // public boolean isSymmetric(TreeNode root) {
+    //     if(root == null) {
+    //         return true;
+    //     }
+    //     return __isSymmetric(root, root);
+    // }
+
+    // private boolean __isSymmetric(TreeNode root1, TreeNode root2) {
+    //     if(root1 == null && root2 == null) {
+    //         return true;
+    //     }
+    //     if(root1 == null || root2 == null) {
+    //         return false;
+    //     }
+
+    //     return (root1.val == root2.val) && __isSymmetric(root1.left, root2.right) && __isSymmetric(root1.right, root2.left);
+
+    // }
+
+
+    //BFS
     public boolean isSymmetric(TreeNode root) {
         if(root == null) {
             return true;
         }
-        return __isSymmetric(root, root);
-    }
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root.left);
+        q.add(root.right);
 
-    private boolean __isSymmetric(TreeNode root1, TreeNode root2) {
-        if(root1 == null && root2 == null) {
-            return true;
+        while(!q.isEmpty()) {
+            TreeNode left = q.poll();
+            TreeNode right = q.poll();
+            if(left == null && right == null) {
+                continue;
+            }
+            if(left == null || right == null) {
+                return false;
+            } 
+            if(left.val != right.val) {
+                return false;
+            }
+            q.add(left.left);
+            q.add(right.right);
+            q.add(left.right);
+            q.add(right.left);
         }
-        if(root1 == null || root2 == null) {
-            return false;
-        }
 
-        return (root1.val == root2.val) && __isSymmetric(root1.left, root2.right) && __isSymmetric(root1.right, root2.left);
-
+        return true;
     }
 }
