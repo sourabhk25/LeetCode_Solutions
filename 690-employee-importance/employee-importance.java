b681e461473021393f72208a191d5f98d651c5f2
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-04-14 21:53:18 -0500

    Time: 4 ms (98.82%) | Memory: 46.9 MB (9.79%) - LeetSync

--- 690-employee-importance/employee-importance.java
+++ 690-employee-importance/employee-importance.java
@@ -14,30 +14,15 @@ class Solution {
         for(Employee e: employees) {
             emap.put(e.id, e);
         }
-        // return dfs(id);
-
-        int res = 0;
-        Queue<Integer> q = new LinkedList<>();
-        q.add(id);
-
-        while (!q.isEmpty()) {
-            int currId = q.poll();
-            Employee currObj = emap.get(currId);
-            res += currObj.importance;
-            for(Integer neigh: currObj.subordinates) {
-                q.add(neigh);
-            }            
-        }
-        return res;
+        return dfs(id);
     }
 
-    // public int dfs(int id) {
-    //     Employee employee = emap.get(id);   //get that employee
-    //     int ans = employee.importance;  //get importance in ans
-    //     for(int subid: employee.subordinates) {
-    //         ans += dfs(subid);  //call dfs on subid
-    //     }
-    //     return ans;
-    // }
-
+    public int dfs(int id) {
+        Employee employee = emap.get(id);   //get that employee
+        int ans = employee.importance;  //get importance in ans
+        for(int subid: employee.subordinates) {
+            ans += dfs(subid);  //call dfs on subid
+        }
+        return ans;
+    }
 }
