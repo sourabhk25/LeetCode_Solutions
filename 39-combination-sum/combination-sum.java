b5cb585c10905e23ea7ade8b2fc9bde0bc8da912
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-04-15 22:45:04 -0500

    Time: 2 ms (82.64%) | Memory: 45 MB (13.99%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -8,26 +8,55 @@ class Solution {
     */
     //using recursion and backtracking
     //here not using condition when target becomes 0 to return, it will add some if condition in code
-    void findCombinations(int index, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
-        //base condition
-        if(index == arr.length) {    //index check
-            if(target == 0) {   //target check
-                ans.add(new ArrayList<>(ds));   //if 0 then add ds to ans
-            }
-            return;  //return
-        }
+    // void findCombinations(int index, int[] arr, int target, List<List<Integer>> ans, List<Integer> ds) {
+    //     //base condition
+    //     if(index == arr.length) {    //index check
+    //         if(target == 0) {   //target check
+    //             ans.add(new ArrayList<>(ds));   //if 0 then add ds to ans
+    //         }
+    //         return;  //return
+    //     }
         
-        if(arr[index] <= target) {   //if value at index is less than target then pick that value
-            ds.add(arr[index]);  //add picked value to ds
-            findCombinations(index, arr, target - arr[index], ans, ds); //stay at same index to check all combinations with same value, new target will be calculated for recurssion  
-            ds.remove(ds.size() - 1);   //remove last element in ds after completing recurssion since it failed if condition
-        } 
-        findCombinations(index + 1, arr, target, ans, ds);   //only index will be incremented for recurssion call
-    }
+    //     if(arr[index] <= target) {   //if value at index is less than target then pick that value
+    //         ds.add(arr[index]);  //add picked value to ds
+    //         findCombinations(index, arr, target - arr[index], ans, ds); //stay at same index to check all combinations with same value, new target will be calculated for recurssion  
+    //         ds.remove(ds.size() - 1);   //remove last element in ds after completing recurssion since it failed if condition
+    //     } 
+    //     findCombinations(index + 1, arr, target, ans, ds);   //only index will be incremented for recurssion call
+    // }
     
+    // public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    //     List<List<Integer>> ans = new ArrayList<>();
+    //     findCombinations(0, candidates, target, ans, new ArrayList<>());
+    //     return ans;
+    // }
+
+    List<List<Integer>> result;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        findCombinations(0, candidates, target, ans, new ArrayList<>());
-        return ans;
+        this.result = new ArrayList<>();
+        helper(candidates, target, 0, new ArrayList<>());
+        return result;
+    }
+
+    void helper(int[] candidates, int target, int i, List<Integer>path) {
+        //base case
+        if(target < 0 || i == candidates.length) {
+            return;
+        }
+
+        if(target == 0) {
+            result.add(new ArrayList<>(path));  //create deep cpy and add to result
+            return;
+        }
+
+        //nochoose
+        helper(candidates, target, i + 1, path);   //send deepcopy of path
+
+        //choose
+        path.add(candidates[i]);
+        helper(candidates, target - candidates[i], i, path);
+
+        //backtracking
+        path.remove(path.size() - 1);
     }
 }
