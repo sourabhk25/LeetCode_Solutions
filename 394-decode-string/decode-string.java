ac818f1378eec93826630a795d3f7b5fe549892c
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-04-26 23:39:35 -0500

    Time: 0 ms (100.00%) | Memory: 41.8 MB (29.04%) - LeetSync

--- 394-decode-string/decode-string.java
+++ 394-decode-string/decode-string.java
@@ -1,27 +1,55 @@
 class Solution {
-    public String decodeString(String s) {
-        Stack<Integer> numSt = new Stack<>();
-        Stack<StringBuilder> strSt = new Stack<>();
+    // public String decodeString(String s) {
+    //     Stack<Integer> numSt = new Stack<>();
+    //     Stack<StringBuilder> strSt = new Stack<>();
 
-        int currNo = 0;
+    //     int currNo = 0;
+    //     StringBuilder currStr = new StringBuilder();
+
+    //     for(int i = 0; i < s.length(); i++) {
+    //         char c = s.charAt(i);
+    //         if(Character.isDigit(c)) {
+    //             currNo = currNo * 10 + c - '0';
+    //         } else if(c == '[') {
+    //             numSt.push(currNo);
+    //             strSt.push(currStr);
+    //             currNo = 0;
+    //             currStr = new StringBuilder();
+    //         } else if(c == ']') {
+    //             int count = numSt.pop();
+    //             StringBuilder parent = strSt.pop();
+    //             for(int j = 0; j < count; j++) {
+    //                 parent.append(currStr);
+    //             }
+    //             currStr = parent;
+    //         } else {
+    //             currStr.append(c);
+    //         }
+    //     }
+
+    //     return currStr.toString();
+    // }
+
+
+    int i = 0;  //index
+    public String decodeString(String s) {
+        int currNum = 0;
         StringBuilder currStr = new StringBuilder();
 
-        for(int i = 0; i < s.length(); i++) {
+        while(i < s.length()) {
             char c = s.charAt(i);
+            i++;
+
             if(Character.isDigit(c)) {
-                currNo = currNo * 10 + c - '0';
+                currNum = currNum * 10 + (c - '0');
             } else if(c == '[') {
-                numSt.push(currNo);
-                strSt.push(currStr);
-                currNo = 0;
-                currStr = new StringBuilder();
-            } else if(c == ']') {
-                int count = numSt.pop();
-                StringBuilder parent = strSt.pop();
-                for(int j = 0; j < count; j++) {
-                    parent.append(currStr);
+                String baby = decodeString(s);
+                for(int k =0; k < currNum; k++) {
+                    currStr.append(baby);
                 }
-                currStr = parent;
+                currNum = 0; //reset number
+            } else if(c == ']') {
+                return currStr.toString();
             } else {
                 currStr.append(c);
             }
