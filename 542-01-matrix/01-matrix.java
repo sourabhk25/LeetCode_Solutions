442ab58e419305c381cf12ddd50696aa7da5a17c
Sourabh Kulkarni <sourabhkulkarni258@gmail.com>
2025-04-26 19:15:21 -0500

    Time: 5 ms (99.86%) | Memory: 49.3 MB (5.34%) - LeetSync

--- 542-01-matrix/01-matrix.java
+++ 542-01-matrix/01-matrix.java
@@ -1,38 +1,101 @@
 class Solution {
     int[][] dirs;
     int m,n;
+    // public int[][] updateMatrix(int[][] mat) {
+    //     this.dirs = new int[][]{{-1,0}, {1,0}, {0,1}, {0,-1}};
+    //     this.m = mat.length;
+    //     this.n = mat[0].length;
+
+    //     Queue<int[]> q = new LinkedList<>();    //stores index of 0s
+
+    //     for(int i = 0; i < m; i++) {
+    //         for(int j = 0; j < n; j++) {
+    //             if(mat[i][j] == 0) {
+    //                 q.add(new int[]{i,j});
+    //             } else {
+    //                 mat[i][j] *= -1;
+    //             }
+    //         }
+    //     }
+
+    //     int dist = 1;
+
+    //     while(!q.isEmpty()) {
+    //         int[] curr = q.poll();
+    //         for(int[] dir: dirs) {
+    //             int r = dir[0] + curr[0];
+    //             int c = dir[1] + curr[1];
+
+    //             if(r >= 0 && c >= 0 && r <m && c < n && mat[r][c] == -1) { //if valid cell  
+    //                 q.add(new int[]{r, c});
+    //                 mat[r][c] = mat[curr[0]][curr[1]] + 1; //distance will be +1 than the neighbor
+    //             }
+    //         }
+    //     }
+
+    //     return mat;
+    // }
+
+    int[][] result;
     public int[][] updateMatrix(int[][] mat) {
         this.dirs = new int[][]{{-1,0}, {1,0}, {0,1}, {0,-1}};
         this.m = mat.length;
         this.n = mat[0].length;
-
-        Queue<int[]> q = new LinkedList<>();    //stores index of 0s
+        this.result = new int[m][n];
 
         for(int i = 0; i < m; i++) {
             for(int j = 0; j < n; j++) {
-                if(mat[i][j] == 0) {
-                    q.add(new int[]{i,j});
-                } else {
-                    mat[i][j] *= -1;
+                if(mat[i][j] == 1) {
+                    result[i][j] =  dfs(mat, i, j);
                 }
             }
         }
 
-        int dist = 1;
+        return result;
+    }
 
-        while(!q.isEmpty()) {
-            int[] curr = q.poll();
-            for(int[] dir: dirs) {
-                int r = dir[0] + curr[0];
-                int c = dir[1] + curr[1];
-
-                if(r >= 0 && c >= 0 && r <m && c < n && mat[r][c] == -1) { //if valid cell  
-                    q.add(new int[]{r, c});
-                    mat[r][c] = mat[curr[0]][curr[1]] + 1; //distance will be +1 than the neighbor
-                }
-            }
+    private int dfs(int[][] mat, int i, int j) {
+        //neigbor is valid and is 0 then distance 1
+        if(i > 0 && mat[i-1][j] == 0) {
+            return 1;
+        }
+        if(i < m-1 && mat[i+1][j] == 0) {
+            return 1;
+        }
+        if(j > 0 && mat[i][j-1] == 0) {
+            return 1;
+        }
+        
+        if(j < n-1 && mat[i][j+1] == 0) {
+            return 1;
         }
 
-        return mat;
+        //keep 4 variables for storing neighbor distances;
+        int top = 9999, left = 9999, bottom = 9999, right = 9999;
+
+        //use memoization
+        if(i > 0 && result[i-1][j] != 0) {
+            top = result[i-1][j];
+        }
+
+        if(j > 0 && result[i][j-1] != 0) {
+            left = result[i][j-1];
+        }
+
+        if(j < n-1) {
+            if(result[i][j+1] == 0) {   //if not calculated already do recursion and calculate value
+                result[i][j+1] = dfs(mat, i, j+1);
+            }
+            right = result[i][j+1]; //else use old value
+        }
+
+        if(i < m-1) {
+            if(result[i+1][j] == 0) {   //if not calculated already do recursion and calculate value
+                result[i+1][j] = dfs(mat, i+1, j);
+            }
+            bottom = result[i+1][j]; //else use old value
+        }
+
+        return Math.min(top, Math.min(left, Math.min(right, bottom))) + 1;
     }
 }
